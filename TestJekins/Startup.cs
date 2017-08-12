a8601ffca0276279eb5ccee4980bf76b4423dcec
leooff <leooff@DESKTOP-8A18RDQ>
2017-08-12 16:59:22 -0300

    Add project files.

---
+++ TestJekins/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestJekins.Startup))]
+namespace TestJekins
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
